4622585994d01d88b1c907bd5f86330a918d019b
Kaito Yagiuchi <Yagisan-k@outlook.jp>
2025-11-07 11:45:10 +0900

    様々な状況をカバー

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2025 Kaito Yagiuchi
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -12,6 +12,14 @@ res=0
 
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
+
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] 	  || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 exit $res
